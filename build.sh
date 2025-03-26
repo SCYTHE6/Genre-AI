b1cffa86a6b8013225390d0e3ccf458882b15079
Divyanshu Chauhan <divyanshuchauhan1283@gmail.com>
2025-03-26 14:21:59 +0530

    Update build script to use npx for next build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Set executable permissions for next binary
-chmod +x node_modules/.bin/next
-
-# Run the build command
-npm run build
+# Run the build command using npx
+npx next build
